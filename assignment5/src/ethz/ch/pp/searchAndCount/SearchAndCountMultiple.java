84a795172091f00d14b1dabcb1702ac9ea204656
Adrian Hirt <aedu.hirt@gmail.com>
2017-03-23 11:42:20 +0100

    parallel code working

--- assignment5/src/ethz/ch/pp/searchAndCount/SearchAndCountMultiple.java
+++ assignment5/src/ethz/ch/pp/searchAndCount/SearchAndCountMultiple.java
@@ -1,13 +1,57 @@
 package ethz.ch.pp.searchAndCount;
-
 import ethz.ch.pp.util.Workload;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 
-public class SearchAndCountMultiple {	
-	
-	//TODO: implement using ForkJoinPool and RecursiveTask
-	public static Integer countNoAppearances(int[] input, int cutOff,
-			Workload.Type wt, int noThreads) {
-		return 0;
+public class SearchAndCountMultiple extends RecursiveTask<Integer>{
+
+	int[] input;
+	Workload.Type workloadType;
+	int cutOff;
+	int threads;
+	int low;
+	int high;
+
+	private SearchAndCountMultiple(int[] input, Workload.Type wt, int co, int noThreads, int low, int high) {
+		this.input = input;
+		this.workloadType = wt;
+		this.cutOff = co;
+		this.threads = noThreads;
+		this.low = low;
+		this.high = high;
 	}
 
+
+	public static Integer countNoAppearances(int[] input, int co, Workload.Type wt, int noThreads) {
+		long t0 = System.currentTimeMillis();
+		SearchAndCountMultiple counter = new SearchAndCountMultiple(input, wt, co, noThreads, 0, input.length);
+		int insertGoodVariableName = ForkJoinPool.commonPool().invoke(counter);
+		long t1 = System.currentTimeMillis();
+		System.out.println("For (inputsize=" + input.length + ",workload=" + wt + ") SearchAndCount Multiple takes "
+				+ (t1 - t0) + " msec");
+
+		return insertGoodVariableName;
+	}
+
+	@Override
+	public Integer compute() {
+		int count = 0;
+		if(high - low <= cutOff) {
+			for (int i = low; i < high; i++) {
+				if (Workload.doWork(input[i], workloadType)){
+					count++;
+				}
+			}
+		}
+		else {
+			int mid = low + (high - low) / 2;
+			SearchAndCountMultiple left = new SearchAndCountMultiple(input, workloadType, cutOff, threads, low, mid);
+			SearchAndCountMultiple right = new SearchAndCountMultiple(input, workloadType, cutOff, threads, mid, high);
+			left.fork();
+			int rightAns = right.compute();
+			int leftAns  = left.join();
+			count = leftAns + rightAns;
+		}
+		return count;
+	}
 }
