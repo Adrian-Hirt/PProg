9246086a6dc7fcadccd091708d581332e7a1085b
Adrian Hirt <aedu.hirt@gmail.com>
2017-03-24 10:37:15 +0100

    Added report for task 1b

--- assignment5/src/ethz/ch/pp/searchAndCount/Main.java
+++ assignment5/src/ethz/ch/pp/searchAndCount/Main.java
@@ -10,8 +10,19 @@ public class Main {
 		DatasetGenerator dg = new DatasetGenerator(100000);
 		int[] input = dg.generate();
 
+		// Standard function executions to test Single vs Multiple
+
 		taskA(input, Workload.Type.HEAVY);
 		taskB(input, 1000, Workload.Type.HEAVY, 4);
+
+		// Test for finding the optimal cutoff and thread count
+
+		for(int i = 100; i <= 1000000; i = i * 10) {
+			for(int j = 2; j <= 128; j = j * 2) {
+				System.out.println("Testing  program with " + i + " cutoff and " + j + " threads");
+				taskB(input, i, Workload.Type.HEAVY, j);
+			}
+		}
 	}
 
 	public static void taskA(int[] input, Workload.Type wt){		
